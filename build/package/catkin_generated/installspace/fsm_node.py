a219ebcbae03c9b8150cb82c1a5066b0c73e0164
陈泓宇 <1729179701@qq.com>
2023-12-22 20:27:43 +0800

    ROS

---
+++ build/package/catkin_generated/installspace/fsm_node.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import rospy
+from sensor_msgs.msg  import Image
+from cv_bridge import CvBridge
+import cv2
+
+def fsm_node(image_msg):
+    bridge=CvBridge()
+    cv_image=bridge.imgmsg_to_cv2(image_msg,desired_encoding="bgr8")
+    gray_image=cv2.cvtColor(cv_image,cv2.COLOR_BGR2GRAY)
+    edges=cv2.Canny(gray_image,50,150)
+    cv2.imshow('edge detection',edges)
+    cv2.waitKey(1)
+
+def main():
+    rospy.init_node("fsm_node",anonymous=True)
+    rospy.Subscriber("camera_image",Image,fsm_node)
+    rospy.spin()
+
+if __name__=="__main__":
+    main()
---
+++ src/package/scripts/camera_node.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+
+def camera_node():
+    rospy.init_node("camera_node", anonymous=True)
+    rate = rospy.Rate(10) # 10Hz
+
+    cap = cv2.VideoCapture(0)
+    pub = rospy.Publisher("camera_image", Image, queue_size=10)
+    bridge = CvBridge()
+
+    while not rospy.is_shutdown():
+        ret, frame = cap.read()
+
+        if ret:
+            cv2.imshow("Camera Image", frame)
+            cv2.waitKey(1) 
+
+            image_msg = bridge.cv2_to_imgmsg(frame, encoding="bgr8")
+            pub.publish(image_msg)
+        rate.sleep()
+
+    cap.release()
+    cv2.destroyAllWindows() 
+
+if __name__ == "__main__":
+    try:
+        camera_node()
+    except rospy.ROSInterruptException:
+        pass
+
